4730113fe22dede3d9cf09a88d0bf1912b359abb
Hieuzest <girkirin@hotmail.com>
2025-07-15 19:16:25 +0800

    feat: implement main

--- packages/core/src/main.rs
+++ packages/core/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Run(RunArgs),
+
+    Start(StartArgs),
+
+    Stop(StopArgs),
+}
+
+#[derive(Args)]
+struct RunArgs {}
+
+#[derive(Args)]
+struct StartArgs {}
+
+#[derive(Args)]
+struct StopArgs {}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Run(_args) => {
+        }
+
+        Commands::Start(_args) => {
+        }
+
+        Commands::Stop(_args) => {
+        }
+    }
+
+    Ok(())
 }
