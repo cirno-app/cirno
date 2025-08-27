242a4f0d4ff2b9d4507067487751489d0adbb9c4
Hieuzest <girkirin@hotmail.com>
2025-08-27 19:18:33 +0800

    feat: update mod server

--- packages/core/src/server.rs
+++ packages/core/src/server.rs
@@ -1,6 +1,30 @@
-use axum::{extract::FromRequestParts, http::request::Parts, response::IntoResponse};
+use axum::{
+    Json, RequestPartsExt,
+    extract::{FromRequest, FromRequestParts},
+    http::request::Parts,
+    response::{IntoResponse, Response},
+};
+use axum_extra::{
+    TypedHeader,
+    headers::{Authorization, authorization::Bearer},
+};
+use log::info;
 use serde::Serialize;
 use std::fmt::Debug;
+use wry::http::StatusCode;
+
+#[derive(FromRequest)]
+#[from_request(via(Json), rejection(ApiError))]
+struct ApiJson<T>(T);
+
+impl<T> IntoResponse for ApiJson<T>
+where
+    Json<T>: IntoResponse,
+{
+    fn into_response(self) -> Response {
+        Json(self.0).into_response()
+    }
+}
 
 #[derive(Debug, Serialize)]
 struct ResponseBody<D>
@@ -29,6 +53,13 @@ where
     type Rejection = ApiError;
 
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        let TypedHeader(Authorization(bearer)) = parts
+            .extract::<TypedHeader<Authorization<Bearer>>>()
+            .await
+            .map_err(|err| {
+                info!("");
+                ApiError::AuthorizationError
+            })?;
         // Return "403 Forbidden" for both authorization and authentication failures,
         // but also log the specific reason for the failure.
         // This prevents attackers from obtaining detailed information,
@@ -45,11 +76,17 @@ struct ErrorResponseBody {
 }
 
 enum ApiError {
+    InvalidJsonError,
     AuthorizationError,
 }
 
 impl IntoResponse for ApiError {
     fn into_response(self) -> axum::response::Response {
-        todo!()
+        let (code, msg) = match self {
+            ApiError::InvalidJsonError => (StatusCode::BAD_REQUEST.as_u16(), "".to_owned()),
+            ApiError::AuthorizationError => (StatusCode::FORBIDDEN.as_u16(), "".to_owned()),
+        };
+
+        (StatusCode::OK, ApiJson(ErrorResponseBody { code, msg })).into_response()
     }
 }
