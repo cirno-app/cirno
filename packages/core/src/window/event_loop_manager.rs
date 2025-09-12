13ae11e8807013b5849870acce787a305050d5f3
Hieuzest <girkirin@hotmail.com>
2025-09-12 11:38:06 +0800

    feat: update `EventLoopManager`

--- packages/core/src/window/event_loop_manager.rs
+++ packages/core/src/window/event_loop_manager.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+
 use tao::event_loop::{ControlFlow::Wait, EventLoop, EventLoopBuilder, EventLoopProxy};
 
 enum EventLoopManagerEvent<T> {
@@ -10,20 +11,42 @@ pub struct EventLoopInit<T: 'static> {
     event_loop: EventLoop<EventLoopManagerEvent<T>>,
 }
 
-pub struct EventLoopManager<T: 'static> {
+struct EventLoopManagerIntl<T: 'static> {
     proxy: EventLoopProxy<EventLoopManagerEvent<T>>,
 }
 
+pub struct EventLoopManager<T: 'static> {
+    intl: Arc<EventLoopManagerIntl<T>>,
+}
+
+pub struct EventLoopClosed<T>(T);
+
 impl<T: 'static> EventLoopManager<T> {
-    pub fn new() -> (EventLoopInit<T>, Arc<EventLoopManager<T>>) {
+    pub fn new() -> (EventLoopInit<T>, EventLoopManager<T>) {
         let event_loop = EventLoopBuilder::<EventLoopManagerEvent<T>>::with_user_event().build();
 
         let proxy = event_loop.create_proxy();
 
         (
             EventLoopInit { event_loop },
-            Arc::new(EventLoopManager::<T> { proxy }),
+            EventLoopManager {
+                intl: Arc::new(EventLoopManagerIntl::<T> { proxy }),
+            },
         )
+    }
+
+    pub fn send_event(&self, event: T) -> Result<(), EventLoopClosed<T>> {
+        match self
+            .intl
+            .proxy
+            .send_event(EventLoopManagerEvent::User(event))
+        {
+            Ok(_) => Ok(()),
+            Err(err) => Err(match err.0 {
+                EventLoopManagerEvent::User(event) => EventLoopClosed(event),
+                _ => panic!(),
+            }),
+        }
     }
 }
 
