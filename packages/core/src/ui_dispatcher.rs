f6b6280178213d5089e13c49c88e953398e244b6
Hieuzest <girkirin@hotmail.com>
2025-09-13 17:58:30 +0800

    feat: implement `Dispatcher`

--- packages/core/src/ui_dispatcher.rs
+++ packages/core/src/ui_dispatcher.rs
@@ -1,13 +1,12 @@
-use std::{pin::Pin, sync::Arc};
-use tao::{
-    event::Event,
-    event_loop::{ControlFlow::Wait, EventLoop, EventLoopBuilder, EventLoopProxy},
+use std::fmt::{Debug, Display, Formatter};
+use std::sync::{Arc, mpsc::sync_channel};
+use tao::event_loop::{
+    ControlFlow::Wait, EventLoop, EventLoopBuilder, EventLoopProxy, EventLoopWindowTarget,
 };
+use thiserror::Error;
 
-enum DispatcherEvent {
-    Dispatch(Box<dyn FnOnce(Event<'_, Self>) -> ()>),
-    DispatchWait(Box<dyn FnOnce(Event<'_, Self>) -> ()>),
-    DispatchAwait(Box<dyn FnOnce(Event<'_, Self>) -> Pin<Box<dyn Future<Output = ()>>>>),
+pub enum DispatcherEvent {
+    Dispatch(Box<dyn FnOnce(&EventLoopWindowTarget<DispatcherEvent>) -> () + Send>),
 }
 
 struct DispatcherIntl {
@@ -15,94 +14,74 @@ struct DispatcherIntl {
 }
 
 pub struct Dispatcher {
-    event_loop: Option<EventLoop<DispatcherEvent>>,
     intl: Arc<DispatcherIntl>,
 }
 
+pub struct DispatcherInit {
+    event_loop: EventLoop<DispatcherEvent>,
+}
+
+#[derive(Error, Debug)]
+#[error("event loop closed")]
 pub struct EventLoopClosed;
 
 impl Clone for Dispatcher {
     fn clone(&self) -> Self {
         Self {
-            event_loop: None,
             intl: self.intl.clone(),
         }
     }
 }
 
 impl Dispatcher {
-    pub fn new() -> Dispatcher {
+    pub fn new() -> (DispatcherInit, Dispatcher) {
         let event_loop = EventLoopBuilder::<DispatcherEvent>::with_user_event().build();
 
         let proxy = event_loop.create_proxy();
 
-        Dispatcher {
-            event_loop: Some(event_loop),
-            intl: Arc::new(DispatcherIntl { proxy }),
-        }
+        (
+            DispatcherInit { event_loop },
+            Dispatcher {
+                intl: Arc::new(DispatcherIntl { proxy }),
+            },
+        )
     }
 
-    pub fn run(self) -> ! {
-        match self.event_loop {
-            None => panic!("Do not call run() on cloned Dispatcher"),
-
-            Some(event_loop) => event_loop.run(|event, event_loop, control_flow| {
-                *control_flow = Wait;
-
-                match event {
-                    tao::event::Event::UserEvent(manager_event) => match manager_event {
-                        DispatcherEvent::Dispatch(fn_once) => todo!(),
-                        DispatcherEvent::DispatchWait(fn_once) => todo!(),
-                        DispatcherEvent::DispatchAwait(fn_once) => todo!(),
-                    },
-                    _ => {}
-                }
-            }),
-        }
-    }
-
-    pub fn dispatch<F: FnOnce(Event<'_, DispatcherEvent>) -> () + 'static>(
+    pub fn dispatch<
+        R: 'static + Send,
+        F: 'static + FnOnce(&EventLoopWindowTarget<DispatcherEvent>) -> R + Send,
+    >(
         &self,
         f: F,
-    ) -> Result<(), EventLoopClosed> {
-        match self
-            .intl
-            .proxy
-            .send_event(DispatcherEvent::Dispatch(Box::new(f)))
-        {
-            Ok(_) => Ok(()),
-            Err(err) => Err(match err.0 {
-                DispatcherEvent::Dispatch(fn_once) => EventLoopClosed {},
-                _ => panic!(),
-            }),
-        }
-    }
+    ) -> Result<R, EventLoopClosed> {
+        let (tx, rx) = sync_channel(0);
 
-    pub fn dispatch_wait<R, F: FnOnce(Event<'_, DispatcherEvent>) -> R>(&self, f: F) {
         match self
             .intl
             .proxy
-            .send_event(DispatcherEvent::Dispatch(Box::new(f)))
-        {
+            .send_event(DispatcherEvent::Dispatch(Box::new(move |event_loop| {
+                tx.send(f(event_loop)).unwrap();
+            }))) {
             Ok(_) => Ok(()),
             Err(err) => Err(match err.0 {
-                DispatcherEvent::Dispatch(fn_once) => EventLoopClosed {},
-                _ => panic!(),
+                DispatcherEvent::Dispatch(_) => EventLoopClosed {},
             }),
-        }
-    }
+        }?;
 
-    pub async fn dispatch_await<R, F: AsyncFnOnce(Event<'_, DispatcherEvent>) -> R>(&self, f: F) {
-        match self
-            .intl
-            .proxy
-            .send_event(DispatcherEvent::Dispatch(Box::new(f)))
-        {
-            Ok(_) => Ok(()),
-            Err(err) => Err(match err.0 {
-                DispatcherEvent::Dispatch(fn_once) => EventLoopClosed {},
-                _ => panic!(),
-            }),
-        }
+        Ok(rx.recv().unwrap())
+    }
+}
+
+impl DispatcherInit {
+    pub fn run(self) -> ! {
+        self.event_loop.run(|event, event_loop, control_flow| {
+            *control_flow = Wait;
+
+            if let tao::event::Event::UserEvent(manager_event) = event {
+                match manager_event {
+                    DispatcherEvent::Dispatch(fn_once) => fn_once(event_loop),
+                }
+            }
+        })
     }
 }
